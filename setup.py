046bbafd1948e19890817a8583c8ca02eccf17fc
Nathan Urwin <nathan.urwin@pgs.com>
2019-01-29 16:05:06 -0600

    Update version in setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="py2log",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     extras_require={"colorlog": "colorlog"},
